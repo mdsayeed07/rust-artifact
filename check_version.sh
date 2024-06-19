0a422d717938acd6d4496b2732e934ae6d6be8b4
RT Sayeed <sayeed2004.18@gmail.com>
2024-06-19 16:07:38 +0530

    adding sanity check

--- check_version.sh
+++ check_version.sh
@@ -10,8 +10,7 @@ RESPONSE=$(curl -s https://crates.io/api/v1/crates/$CRATE_NAME)
 
 if echo "$RESPONSE" | jq -e ".versions | map(select(.num == \"$CRATE_VERSION\")) | length > 0" > /dev/null; then
   echo "Crate $CRATE_NAME version $CRATE_VERSION already exists on crates.io. Skipping publish."
-  exit 0
+  exit 1
 else
   echo "Crate $CRATE_NAME version $CRATE_VERSION does not exist on crates.io. Proceeding to publish."
-  exit 1
 fi
